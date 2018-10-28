1be18b8e3500425885a43ef5f72cee26d987ebf0
calvin5338 <44057469+calvin5338@users.noreply.github.com>
2018-10-28 15:56:45 +0800

    Finish !

--- money.cpp
+++ money.cpp
@@ -1,2 +1,13 @@
 #include <iostream>
 using namespace std;
+
+int main() {
+  int money;
+  cout << "請輸入逾換算金額:";
+  cin >> money;
+  
+  cout <<">> 50元有:" << money/50 <<"個\n";
+  cout <<"   10元有:" << money%50/10 <<"個\n";
+  cout <<"    5元有:" << money%50%10/5 <<"個\n";
+  cout <<"    1元有:" << money%50%10%5 <<"個";
+  }
